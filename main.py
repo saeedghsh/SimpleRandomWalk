8aabdb0cbc5d14393659588f036691c122d6f992
Saeed Gholami Shahbandi <2572507+saeedghsh@users.noreply.github.com>
2020-06-23 00:29:56 +0200

    Add requirements for pip dependencies

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+"""
+Simple Random Walk - main script
+"""
 #!/usr/bin/env python
 from __future__ import print_function
 
 from SimpleRandomWalk.SimpleRandomWalk import SimpleRandomWalk
 from SimpleRandomWalk.plotting import SlidingFigure
 
-count  = 1
-length = 100
-
-random_walks = [SimpleRandomWalk(length) for _ in range(count)]
-sf = SlidingFigure(random_walks)
+if __name__ == "__main__":
+    
+    count  = 1
+    length = 100
+    
+    random_walks = [SimpleRandomWalk(length) for _ in range(count)]
+    sf = SlidingFigure(random_walks)
